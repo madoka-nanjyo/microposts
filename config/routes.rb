a4762929859a931be52106378fa47d96ffc4ff75
madoka nanjyo <madoka-nanjyo@united.jp>
2016-04-16 11:53:07 +0000

    kadai1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-root to: 'static_pages#home'
+  get 'sessions/new'
+
+  root to: 'static_pages#home'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get    'setting' , to: 'sessions#edit'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy, :edit, :update]
 end
