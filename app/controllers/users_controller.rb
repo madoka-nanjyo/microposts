2f589f4caae0cda4dd93b6dc7e1075a1e76c9e2c
madoka nanjyo <madoka-nanjyo@united.jp>
2016-04-19 11:37:09 +0000

    upp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :followings, :followers]
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
     before_action :correct_user,   only: [:edit, :update]
    
   def show
@@ -34,15 +34,17 @@ class UsersController < ApplicationController
     end
   end
 
-  def followings
+  def following
+    @user = User.find(params[:id])
     @title = "Following"
-    @users = @user.following_users.paginate(page: params[:page], :per_page => 5)
+    @users = @user.following_users
     render 'show_follow'
   end
 
   def followers
+    @user = User.find(params[:id])
     @title = "Followers"
-    @users = @user.follower_users.paginate(page: params[:page], :per_page => 5)
+    @users = @user.follower_users
     render 'show_follow'
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,16 @@ Rails.application.routes.draw do
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get    'setting' , to: 'sessions#edit'
-  get    'profile' ,to:'users#followers'
-  get    'profile' ,to:'users#followings'
+   resources :users do
+   member do
+     get :following, :followers
+   end
+ end
+ 
+
   
   resources :users
   resources :sessions, only: [:new, :create, :destroy, :edit, :update]
   resources :microposts
   resources :relationships, only: [:create, :destroy]
-  resources :following_users
-  resources :follower_users
 end
