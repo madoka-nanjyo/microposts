c7c0c7be5257a22a18aaecba9b85b05d142b38b5
madoka nanjyo <madoka-nanjyo@united.jp>
2016-04-21 05:40:54 +0000

    1234

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160419073337) do
   end
 
   add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
