5e1f76871e8b7bcdb4d563c5688d3199ce9a0c0c
madoka nanjyo <madoka-nanjyo@united.jp>
2016-04-21 08:53:05 +0000

    page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_use.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(10)
       render 'static_pages/home'
     end
   end
@@ -24,3 +25,4 @@ class MicropostsController < ApplicationController
     params.require(:micropost).permit(:content)
   end
 end
+
